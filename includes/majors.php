ab1269a3599bd0c63d02124b77a46fc2ef5e2a42
wanwanvxt <119155820+wanwanvxt@users.noreply.github.com>
2024-01-27 14:58:55 +0700

    update 2024.1.27.2

--- includes/majors.php
+++ includes/majors.php
@@ -20,24 +20,24 @@ function getMajorInfoByUniver($major_id, $univer_id)
 {
   $db = new database();
   // major_id, name, block, score
-  return $db->execute("select u_m.major_id, majors.name, u_m.block, u_m.score from u_m join majors on u_m.major_id = majors.id where u_m.major_id=? and u_m.univer_id=?", "ss", $major_id, $univer_id);
+  return $db->execute("select u_m.major_id, majors.name, u_m.block, u_m.score from u_m join majors on u_m.major_id = majors.id where u_m.major_id=? and u_m.univer_id=?", "is", $major_id, $univer_id);
 }
 
 function addMajorByUniver($univer_id, $major_id, $block, $score)
 {
   $db = new database();
-  return $db->execute("insert into u_m (univer_id, major_id, block, score) values (?,?,?,?)", "sssd", $univer_id, $major_id, $block, $score);
+  return $db->execute("insert into u_m (univer_id, major_id, block, score) values (?,?,?,?)", "sisd", $univer_id, $major_id, $block, $score);
 }
 
 function updateMajorByUniver($major_id, $univer_id, $block, $score)
 {
   $db = new database();
-  return $db->execute("update u_m set block=?, score=? where major_id=? and univer_id=?", "ssss", $block, $score, $major_id, $univer_id);
+  return $db->execute("update u_m set block=?, score=? where major_id=? and univer_id=?", "sdis", $block, $score, $major_id, $univer_id);
 }
 
 function deleteMajorByUniver($major_id, $univer_id)
 {
   $db = new database();
-  return $db->execute("delete from u_m where major_id=? and univer_id=?", "ss", $major_id, $univer_id);
+  return $db->execute("delete from u_m where major_id=? and univer_id=?", "is", $major_id, $univer_id);
 }
 #endregion
